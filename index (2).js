a29f4b61274d3370047d1931b4fee66100f08443
Sammar Juneja <70101180+SammarJuneja@users.noreply.github.com>
2024-07-01 14:25:18 +0530

    Add files via upload

---
+++ index (2).js
@@ -0,0 +1,26 @@
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+client.modals = new Collection();
+client.buttons = new Collection();
+client.cooldowns = new Collection();
+client.commandArray = [];
+
+const functionFolders = fs.readdirSync("./src/functions");
+for (const folder of functionFolders) {
+const functionFiles = fs.readdirSync(`./src/functions/${folder}`).filter((file) => file.endsWith(".js"));
+for (const file of functionFiles) {
+    require(`./src/functions/${folder}/${file}`) (client);
+}
+}
+
+client.handleCommands();
+client.handleEvents();
+client.handleComponents();
+
+client.login('OTM2NzEwNjcxMDcxMDgwNTMw.GCmgeV.cyDgtjc6uYaEnPNvpE26EVdiNAhZDa5HCvVKwk');
+console.log('RUNNING');
